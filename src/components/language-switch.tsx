a7ed8f9e38f76c1626a510ec8f248c6b0e6fc4b3
æLtorio <aeltorio@universidadatlantico.org>
2025-03-18 08:32:19 +0100

    feat: enhance language switch component with improved metadata handling and RTL support add rtl demo with ar-SA and he-IL languages

--- src/components/language-switch.tsx
+++ src/components/language-switch.tsx
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-import { type FC, useState, useEffect } from "react";
+import { type FC, useState, useEffect, useCallback } from "react";
 import { useTranslation } from "react-i18next";
 import {
   Dropdown,
@@ -24,69 +24,125 @@ import {
   DropdownItem,
 } from "@heroui/dropdown";
 import { Button } from "@heroui/button";
+import { Tooltip } from "@heroui/tooltip";
 
 import { availableLanguages } from "@/i18n";
 import { I18nIcon } from "@/components/icons";
 
+/**
+ * Language switch component
+ * @description
+ * A language switch component that allows users to change the language of the application
+ * It uses the i18n instance to change the language and update the document metadata
+ * Available languages are defined in the i18n configuration (src/i18n.ts)
+ * @example
+ * ```tsx
+ * <LanguageSwitch />
+ * ```
+ */
 export const LanguageSwitch: FC = () => {
   const { i18n, t } = useTranslation();
-  const [language, setLanguage] = useState<string>(i18n.language);
+  const [language, setLanguage] = useState<string>(
+    localStorage.getItem("preferredLanguage") || i18n.language,
+  );
 
+  /**
+   * Update document direction based on language
+   * @description
+   * This effect updates the document direction based on the language
+   * It uses the availableLanguages array to determine the language direction
+   * @see availableLanguages
+   */
+  useEffect(() => {
+    const isRTL =
+      availableLanguages.find((lang) => lang.code === language)?.isRTL || false;
+
+    document.documentElement.dir = isRTL ? "rtl" : "ltr";
+  }, [language]);
+
+  // Sync state with i18n when language changes externally
   useEffect(() => {
     setLanguage(i18n.language);
   }, [i18n.language]);
 
-  const changeLanguage = (lng: string) => {
-    i18n.changeLanguage(lng);
-    setLanguage(lng);
-    document.documentElement.lang = lng;
-    document.title = t("vite-heroui");
-    document.head
-      .querySelector("meta[key='title']")
-      ?.setAttribute("content", t("vite-heroui"));
-    document.head
-      .querySelector("meta[name='title']")
-      ?.setAttribute("content", t("vite-heroui"));
-  };
+  /**
+   * Change the language and update document metadata
+   * @param lng The language code
+   * @example changeLanguage("fr-FR")
+   */
+  const changeLanguage = useCallback(
+    (lng: string) => {
+      i18n.changeLanguage(lng);
+      setLanguage(lng);
+      localStorage.setItem("preferredLanguage", lng);
+      document.documentElement.lang = lng;
+
+      // Update metadata
+      document.title = t("vite-heroui");
+      const metaTags = [
+        document.head.querySelector("meta[key='title']"),
+        document.head.querySelector("meta[name='title']"),
+        document.head.querySelector("meta[property='og:title']"),
+      ];
+
+      metaTags.forEach((tag) => {
+        tag?.setAttribute("content", t("vite-heroui"));
+      });
+    },
+    [i18n, t],
+  );
 
   /**
-   * Get the short language code
+   * Get the short language code from the language code
+   * erases the region part of the language code
    * @param lng
-   * @returns
+   * @returns The short language code
+   * @example getShortLanguage("fr-FR") => "FR"
    */
   const getShortLanguage = (lng: string) => {
-    // use the language code for Chinese
-    if (lng.startsWith("zh")) {
-      return "中文";
-    }
-
-    // use the first part of the language code
-    return lng.split("-")[0];
+    // use the last part of the language code or the whole code
+    return lng.split("-")[1] || lng;
   };
 
   return (
-    <div className="flex gap-1">
-      <Dropdown>
-        <DropdownTrigger>
-          <Button aria-label={t("language")} variant="light">
-            <I18nIcon className="text-default-500" size={24} />
-          </Button>
-        </DropdownTrigger>
-        <DropdownMenu>
-          {availableLanguages.map((lng) => (
-            <DropdownItem key={lng} aria-label={`${t("language")}: ${lng}`}>
-              <button
-                key={lng}
-                className={`${language === lng ? "text-primary" : "text-default-600"} w-full`}
-                type="button"
-                onClick={() => changeLanguage(lng)}
-              >
-                {getShortLanguage(lng).toLocaleUpperCase()}
-              </button>
-            </DropdownItem>
-          ))}
-        </DropdownMenu>
-      </Dropdown>
-    </div>
+    <Tooltip content={t("language")} delay={750}>
+      <div className="flex gap-1">
+        <Dropdown>
+          <DropdownTrigger>
+            <Button aria-label={t("language")} variant="light">
+              <I18nIcon className="text-default-500" size={24} />
+            </Button>
+          </DropdownTrigger>
+          <DropdownMenu aria-label={t("language")}>
+            {availableLanguages.map((languageIdentifier) => {
+              // Construct the language switch component with the language code
+              const isSelected = language === languageIdentifier.code;
+
+              return (
+                <DropdownItem
+                  key={languageIdentifier.code}
+                  aria-label={`${t("language")}: ${languageIdentifier}`}
+                  aria-selected={isSelected}
+                >
+                  <button
+                    key={languageIdentifier.code}
+                    className={`${isSelected ? "text-primary" : "text-default-600"} w-full flex items-center justify-between`}
+                    type="button"
+                    onClick={() => changeLanguage(languageIdentifier.code)}
+                  >
+                    <span>{languageIdentifier.nativeName}</span>
+                    <span>
+                      {getShortLanguage(
+                        languageIdentifier.code,
+                      ).toLocaleUpperCase()}
+                    </span>
+                  </button>
+                </DropdownItem>
+              );
+            })}
+          </DropdownMenu>
+        </Dropdown>
+      </div>
+    </Tooltip>
   );
 };
--- src/i18n.ts
+++ src/i18n.ts
@@ -1,8 +1,42 @@
+/**
+ * i18n configuration
+ * @description
+ * This file configures the i18n instance with the following settings:
+ *  * The default language is English (en-US)
+ *  * The available languages are defined in the availableLanguages array
+ *  * The i18n instance is initialized with the i18next-http-backend plugin
+ *  * The i18n instance is exported as the default export
+ * How to add a new language ?
+ *  * Add a new object to the availableLanguages array with the following properties:
+ *    * code: The ISO 639-1 language code (e.g., "en-US")
+ *    * nativeName: The native name of the language (e.g., "English")
+ *    * isRTL: Whether the language is right-to-left (e.g., false)
+ *  * Create a new JSON file in the locales/{namespace} directory with the language code (e.g., "en-US.json")
+ *  * Manually add a switch case to the loadPath function in the i18n configuration to load the new JSON file
+ *  * The new language should now be available for selection in the LanguageSwitch component
+ * @see src/components/language-switch.tsx
+ * @see src/locales/base/en-US.json
+ */
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import i18nextHttpBackend, {
   type HttpBackendOptions,
 } from "i18next-http-backend";
+
+export interface AvailableLanguage {
+  code: string; // ISO 639-1 language code
+  nativeName: string; // Native name of the language
+  isRTL: boolean; // Right-to-left language
+}
+
+export const availableLanguages: AvailableLanguage[] = [
+  { code: "en-US", nativeName: "English", isRTL: false },
+  { code: "fr-FR", nativeName: "Français", isRTL: false },
+  { code: "es-ES", nativeName: "Español", isRTL: false },
+  { code: "zh-CN", nativeName: "中文", isRTL: false },
+  { code: "ar-SA", nativeName: "العربية", isRTL: true },
+  { code: "he-IL", nativeName: "עברית", isRTL: true },
+];
 
 i18n
   .use(i18nextHttpBackend)
@@ -24,6 +58,7 @@ i18n
 
         // Vite does not know how to resolve
         // new URL(`./locales/${ns}/${reqlng}.json`, import.meta.url)
+        // so we need to manually handle the different namespaces and languages
         switch (ns[0]) {
           case "base":
             switch (lng[0]) {
@@ -39,6 +74,12 @@ i18n
               case "zh-CN":
                 url = new URL("./locales/base/zh-CN.json", import.meta.url);
                 break;
+              case "ar-SA":
+                url = new URL("./locales/base/ar-SA.json", import.meta.url);
+                break;
+              case "he-IL":
+                url = new URL("./locales/base/he-IL.json", import.meta.url);
+                break;
               default:
                 url = new URL("./locales/base/en-US.json", import.meta.url);
             }
@@ -53,4 +94,3 @@ i18n
   });
 
 export default i18n;
-export const availableLanguages = ["en-US", "fr-FR", "es-ES", "zh-CN"];
