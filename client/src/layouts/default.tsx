6c7d2c99972c90fdd4010adee65143af757318a6
Ronan L <aeltorio@universidadatlantico.org>
2025-08-09 12:59:18 +0200

    feat: add user authentication details and token management in DefaultLayout feat: update translations for authentication-related terms in multiple languages

--- client/src/layouts/default.tsx
+++ client/src/layouts/default.tsx
@@ -4,6 +4,11 @@ import { Link } from "@heroui/link";
 import { Trans, useTranslation } from "react-i18next";
 
 import { Navbar } from "@/components/navbar";
+import { Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from "@heroui/dropdown";
+import { Snippet } from "@heroui/snippet";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect, useState } from "react";
+import { createRemoteJWKSet, JWTPayload, jwtVerify } from "jose";
 
 export default function DefaultLayout({
   children,
@@ -11,6 +16,29 @@ export default function DefaultLayout({
   children: React.ReactNode;
 }) {
   const { t } = useTranslation();
+  const { isAuthenticated, user, getAccessTokenSilently } = useAuth0();
+  const [accessToken, setAccessToken] = useState<string | null>(null);
+  const [decodedToken, setDecodedToken] = useState<JWTPayload | null>(null);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      getAccessTokenSilently().then((token) => {
+        setAccessToken(token);
+        const JWKS = createRemoteJWKSet(
+          new URL(
+            `https://${import.meta.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
+          ),
+        );
+
+        jwtVerify(token, JWKS, {
+          issuer: `https://${import.meta.env.AUTH0_DOMAIN}/`,
+          audience: import.meta.env.AUTH0_AUDIENCE,
+        }).then((jwt) => {
+          setDecodedToken(jwt.payload as JWTPayload);
+        });
+      });
+    }
+  }, [isAuthenticated]);
 
   return (
     <div className="relative flex flex-col h-screen">
@@ -29,7 +57,40 @@ export default function DefaultLayout({
             <Trans ns="base">powered-by</Trans>
           </span>
           <p className="text-primary">HeroUI</p>
-        </Link>
+        </Link>&nbsp;
+        <Dropdown>
+          <DropdownTrigger>
+            {isAuthenticated ? (
+              <span>
+                {t("user")}: &nbsp;{user?.name}
+              </span>
+            ) : (
+              <></>
+            )}
+          </DropdownTrigger>
+          <DropdownMenu className="max-w-5xl">
+            <DropdownItem key="user-logged" textValue="user-logged">
+              <span className="text-default-600">{t("token")}:</span>
+              <br />
+              <Snippet className="max-w-4xl" symbol="" title="api-response">
+                <div className="max-w-2xs sm:max-w-sm md:max-w-md lg:max-w-3xl  whitespace-break-spaces  text-wrap break-words">
+                  {accessToken}
+                </div>
+              </Snippet>
+              <br />
+              <span className="text-default-600">
+                {t("expiration")}:{" "}
+                {new Date((decodedToken?.exp || 0) * 1000).toLocaleString()}
+              </span>
+              <br />
+              <span className="text-default-600">
+                {t("permissions")}:{" "}
+                {((decodedToken?.permissions as string[]) || []).join(", ") ||
+                  t("no-permissions")}
+              </span>
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
       </footer>
     </div>
   );
