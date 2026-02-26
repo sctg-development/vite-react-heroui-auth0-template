a473e83bb9ebf9122551cdf8729936d495b2ef24
Ronan L <aeltorio@universidadatlantico.org>
2026-02-26 15:37:41 +0100

    bug: need to instanciate ToastProvider

--- apps/client/src/provider.tsx
+++ apps/client/src/provider.tsx
@@ -20,6 +20,7 @@ import type React from "react";
 import type { NavigateOptions } from "react-router-dom";
 
 import { HeroUIProvider } from "@heroui/system";
+import { ToastProvider } from "@heroui/toast";
 import { useHref, useNavigate } from "react-router-dom";
 
 declare module "@react-types/shared" {
@@ -33,6 +34,7 @@ export function Provider({ children }: { children: React.ReactNode }) {
 
   return (
     <HeroUIProvider navigate={navigate} useHref={useHref}>
+      <ToastProvider />
       {children}
     </HeroUIProvider>
   );
